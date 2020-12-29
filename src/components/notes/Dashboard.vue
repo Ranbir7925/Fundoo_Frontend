9f3d612d42bea6f20f094e4066a7389c1e75a9f2
Ranbir Singh <honeyranbir7925@gmail.com>
2020-12-29 22:01:03 +0530

    add:New component to show home page ui

---
+++ src/components/notes/Dashboard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <NavBar />
+    <!-- <Notes /> -->
+  </div>
+</template>
+
+<script>
+import NavBar from "../notes/NavBar"
+// import Notes from "../notes/Notes"
+export default {
+    name:"Dashboard",
+    components:{NavBar}
+    
+}
+</script>
