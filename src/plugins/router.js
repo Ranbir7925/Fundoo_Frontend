75b1d1ffa82ed656585b6ebced7e646cf893c23b
Ranbir Singh <honeyranbir7925@gmail.com>
2020-12-29 22:04:48 +0530

    add:Child routing done for notes component

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -1,16 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import SignIn from '../components/SignIn'
-import SignUp from '../components/SignUp'
+import SignIn from '../components/users/SignIn'
+import SignUp from '../components/users/SignUp'
+import ForgotPassword from '../components/users/ForgotPassword'
+import ResetPassword from '../components/users/ResetPassword'
+import Dashboard from '../components/notes/Dashboard'
+import Notes from '../components/notes/Notes'
 
 Vue.use(Router)
 
 const router = new Router({
   mode: 'history',
   routes: [
-    { path: '/user/signin', component: SignIn },
-    { path: '/user/signup', component: SignUp },
-    { path: '*', component: SignIn }
+    { path: '*', redirect: '/signin' },
+    { path: '/signin', component: SignIn },
+    { path: '/signup', component: SignUp },
+    { path: '/forgotpassword', component: ForgotPassword },
+    { path: '/resetpassword/:token', component: ResetPassword },
+    {
+      path: '/dashboard', component: Dashboard,
+      children: [
+        { path: 'notes', component: Notes }
+      ]
+    },
   ]
 });
 
