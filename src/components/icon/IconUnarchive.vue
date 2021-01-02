51717e5fe98a6994761326734f1edd30b909e92b
Ranbir Singh <honeyranbir7925@gmail.com>
2021-01-02 19:12:20 +0530

    Code to show notes after unarchiveing

--- src/components/icon/IconUnarchive.vue
+++ src/components/icon/IconUnarchive.vue
@@ -1,12 +1,27 @@
 <template>
   <div class="unarchive">
-    <md-icon>unarchive</md-icon>
+    <md-icon @click.native="unarchiveNotes">unarchive</md-icon>
   </div>
 </template>
 
 <script>
+import NoteService from '../../services/noteService'
+import { eventBus } from '../../main'
 export default {
   name: "IconUnarchive",
+  props:['noteId'],
+  methods:{
+      unarchiveNotes:function(){
+          const noteData = {
+              isArchived: false,
+        noteIdList: [this.noteId],
+          }
+          NoteService.unarchiveNotes(noteData)
+          .then(()=>{
+              eventBus.$emit("unarchivedNote")
+          })
+      }
+  }
 };
 </script>
 
